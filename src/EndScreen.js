20a9d3619e6f6760fbf68d8fec80f706e8cf6741
Aanish Sachdev <aanishsachdev@gmail.com>
2024-05-26 21:40:55 +0530

    share button

--- src/EndScreen.js
+++ src/EndScreen.js
@@ -7,6 +7,7 @@ const EndScreen = () => {
     const gameUrl = `https://playwavelength.com/join/${gameId}`;
     const [linkText, setLinkText] = useState(gameUrl);
     const navigate = useNavigate();
+    const canShare = navigator.canShare;
 
     const handleCopyClick = () => {
         navigator.clipboard.writeText(gameUrl).then(() => {
@@ -15,6 +16,22 @@ const EndScreen = () => {
                 setLinkText(gameUrl);
             }, 2000);
         });
+    };
+
+    const handleShareClick = () => {
+        if (canShare) {
+            navigator.share({
+                title: 'Join me on Wavelength!',
+                text: 'Play Wavelength with me!',
+                url: gameUrl,
+            }).then(() => {
+                console.log('Thanks for sharing!');
+            }).catch((error) => {
+                console.error('Error sharing', error);
+            });
+        } else {
+            console.error('Your browser does not support native sharing');
+        }
     };
 
     const handleSeeFriendsClick = () => {
@@ -28,9 +45,19 @@ const EndScreen = () => {
             <div className="link-box" onClick={handleCopyClick}>
                 {linkText}
             </div>
+            {canShare ? (
+                <button onClick={handleShareClick} className="share-button">
+                    Share
+                    <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 100 100" fill="currentColor" style={{ marginLeft: '4px' }}>
+                        <g>
+                            <path d="M75 64c-4.8 0-9.1 2.3-11.8 5.8L38.9 55.7c1.5-3.7 1.5-7.8 0-11.5l24.3-14.1c2.7 3.5 7 5.8 11.8 5.8 8.3 0 15-6.7 15-15s-6.7-15-15-15-15 6.7-15 15c0 2 .4 4 1.1 5.7L36.8 40.8C34 37.3 29.8 35 25 35c-8.3 0-15 6.7-15 15s6.7 15 15 15c4.8 0 9.1-2.3 11.8-5.8l24.3 14.1C60.4 75 60 77 60 79c0 8.3 6.7 15 15 15s15-6.7 15-15-6.7-15-15-15zm0-54c6.1 0 11 4.9 11 11s-4.9 11-11 11-11-4.9-11-11 4.9-11 11-11zM25 61c-6.1 0-11-4.9-11-11s4.9-11 11-11 11 4.9 11 11-4.9 11-11 11zm50 29c-6.1 0-11-4.9-11-11s4.9-11 11-11 11 4.9 11 11-4.9 11-11 11z"></path>
+                        </g>
+                    </svg>
+                </button>
+            ) : null} 
             <button className="custom-button" onClick={handleSeeFriendsClick}>
                 See how your friends did
-            <span className="button-arrow">{'>'}</span>
+                <span className="button-arrow">{'>'}</span>
             </button>
         </div>
     );
