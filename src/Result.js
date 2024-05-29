04251a4800184c1b31be36c9be94e5513808cb36
Aanish Sachdev <aanishsachdev@gmail.com>
2024-05-30 00:35:30 +0530

    added text

--- src/Result.js
+++ src/Result.js
@@ -80,17 +80,17 @@ const Result = () => {
         if (score == 1) {
             setFadeText('Opposite Poles Attract!');
         } else if (score == 2) {
-            setFadeText('Finding Common Ground!');
+            setFadeText('Different Beats!');
         } else if (score == 3) {
-            setFadeText('Connecting More!');
+            setFadeText('Merging Paths!');
         } else if (score == 4) {
-            setFadeText('Growing Together!');
+            setFadeText('Bridging Views!');
         } else if (score == 5) {
             setFadeText('Syncing Up!');
         } else if (score == 6) {
             setFadeText('Getting in Tune!');
         } else if (score == 7) {
-            setFadeText('Harmonizing Beautifully!');
+            setFadeText('Melodic Match!');
         } else if (score == 8) {
             setFadeText('In Perfect Rythm!');
         } else if (score == 9) {
@@ -101,7 +101,7 @@ const Result = () => {
         
         if (score !== -1) {
             setTimeout(() => {
-                setShowFadeText(true); // This will trigger the text to fade in after 1 second
+                setShowFadeText(true);
             }, 1000);
         }
     }, [score]);
@@ -115,8 +115,21 @@ const Result = () => {
         borderRadius: '5px',
         zIndex: '1',
         position: 'relative',
-        marginTop: '15px'
+        marginTop: '5px'
     };
+
+    const scoreExplanations = {
+        1: "Even though your answers greatly differ, there's something exciting about how distinct your perspectives are. Opposites can bring new insights and growth opportunities to each other.",
+        2: "You've found a few common notes, indicating a budding understanding and shared views in a couple of areas. There's room to learn more about each other's unique rhythms.",
+        3: "Your answers are starting to align, showing that you're on the path to understanding each other better. Your perspectives are more alike than different, paving the way for deeper connections.",
+        4: "You are effectively bridging your differences, finding more in common than you may have initially thought. Your views are aligning, building a strong foundation for mutual understanding.",
+        5: "You are increasingly in sync, sharing several common opinions and perspectives. This level of agreement shows a solid understanding and similarity in your viewpoints.",
+        6: "Your answers are harmonizing well! You're tuning into each other's frequencies, suggesting a promising potential for a strong connection and shared values.",
+        7: "Your responses harmonize like a well-composed melody, indicating a deep and resonant connection in your thoughts and values. You're thinking alike in many wonderful ways.",
+        8: "You're completely in step with each other, showing that your thoughts and feelings move beautifully together. It's a dance of perfect agreement and mutual understanding.",
+        9: "Your responses are nearly identical, revealing that you share a special bond and deep understanding. You truly are kindred spirits, thinking and feeling in remarkable harmony.",
+        10: "Perfect score! Your answers align perfectly, suggesting an extraordinary level of compatibility and understanding. It's as if you were meant to think and feel together."
+    };    
 
     if (error) return <div>Error: {error}</div>;
 
@@ -132,13 +145,17 @@ const Result = () => {
                     <div style={{ display: 'flex', justifyContent: 'center' }}>
                         <DonutProgressBar percentage={score * 10} />
                     </div>
-                    
                 )}
                 <div className={`score-text ${showFadeText ? 'fade-in' : ''}`}>
                     {fadeText}
                 </div>
+                {score !== -1 && (
+                    <div className="scorecard">
+                        {scoreExplanations[score]}
+                    </div>
+                )}
                 <button style={buttonStyle} onClick={() => navigate('/')}>
-                    Play With Your Friends
+                    Play A New Game With Your Friends
                 </button>
             </div>
             <div className="r-line r-line-one"></div>
@@ -155,6 +172,7 @@ const Result = () => {
             <div className="r-line r-line-twelve"></div>
         </div>
     );
+    
 };
 
 export default Result;
